d0168acb219338ce86976e7a4e6f3c3c25c391ff
Rafael Montes <rafaelmontes1997@gmail.com>
2020-04-02 16:45:38 -0400

    added movies file in views

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>
-      <router-link to="/qwer">Movies</router-link>
+      <router-link to="/movies">Movies</router-link>
       <router-link to="/">Review</router-link>
     </div>
     <img src="./assets/logo.png">
