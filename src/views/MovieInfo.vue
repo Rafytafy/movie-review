0afa8d85ddb96d75c666b954490da3840ee3b05e
Danielson9712 <55254935+Danielson9712@users.noreply.github.com>
2020-04-15 22:59:46 -0400

    movie review page

--- src/views/MovieInfo.vue
+++ src/views/MovieInfo.vue
@@ -1,10 +1,37 @@
 <template>
   <div>
     {{ $route.params.movie }}
+    <div class="text">
+      <h1>Genre</h1>
+      <h3>Thriller</h3>
+      <br />
+      <h1>Year</h1>
+      <p1>1994</p1>
+      <br />
+      <h1>Score</h1>
+      <h3>5</h3>
+      <br />
+      <h1>Review</h1>
+      <h3>Date</h3>
+      <p1>April 15, 2020</p1>
+      <br />
+      <p1>Review goes here</p1>
+    </div>
   </div>
 </template>
 
-<style> 
-
-
+<style>
+.text {
+  text-align: center;
+}
+h1,
+h2 {
+  background-color: #af0404;
+  background-size: 50% 50%;
+  text-decoration: underline;
+  text-decoration-color: gray;
+  border: black 1px;
+  margin-right: 300px;
+  margin-left: 300px;
+}
 </style> 
